36dd58e478ddb1a44c12153d581d387019310da1
Ylber Xhambazi <ylber.xhambazi@ndbit.net>
2021-12-15 16:42:58 +0100

    fix: header and refresh responsive layout

--- js/script.js
+++ js/script.js
@@ -5,5 +5,24 @@ toggle.addEventListener('click', function () {
     navLinks.classList.toggle('active');
 });
 
-var element = document.querySelector("#home");
-element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+
+var btn = document.querySelector('.scrollToTop');
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+btn.addEventListener("click", topFunction());
